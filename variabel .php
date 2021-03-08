94041e967dc5a5b668853ab9308514c9ad8f39c4
Ulil albab <79692710+ulilalbab05@users.noreply.github.com>
2021-03-08 08:37:39 +0700

    Add files via upload

---
+++ variabel .php
@@ -0,0 +1,13 @@
+<?php
+// kita akan membuat sebuah variable dalam php
+
+$namadepan = "ulil";
+$namabelakang = "albab";
+$jumlahbarang = 3;
+$harga = 100000;
+
+
+echo "nama saya adalah $namadepan $namabelakang saya membeli buah naga sebanyak $jumlahbarang seharga $harga";
+
+
+?>
